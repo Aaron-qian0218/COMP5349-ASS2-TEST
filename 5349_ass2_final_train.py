2620dcecddbe76634aa6e2604bd60df4ee14e298
Aaron-qian0218 <66901369+Aaron-qian0218@users.noreply.github.com>
2022-05-26 14:16:29 +0800

    Add files via upload

--- 5349_ass2_final_train.py
+++ 5349_ass2_final_train.py
@@ -14,7 +14,8 @@ spark = SparkSession \
     .builder \
     .appName("COMP5349 Assignment2 Dataset")\
     .config("spark.executor.memory", "4g")\
-    .config("spark.driver.memory", "6g")\
+    .config("spark.driver.memory", "6g") \
+    .config("spark.executor.cores", "4") \
     .config("spark.sql.inMemoryColumnarStorage.compressed", "true")\
     .config("spark.sql.execution.arrow.enabled", "true")\
     .getOrCreate()
