cf1a05deed64cb54a8ccc4451c96048b5f04bffb
Aaron-qian0218 <66901369+Aaron-qian0218@users.noreply.github.com>
2022-05-26 13:29:16 +0800

    Add files via upload

---
+++ submit_client_test.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode client \
+    5349_ass2_final_test.py \
+    --output $1
---
+++ submit_cluster_test.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode cluster \
+    5349_ass2_final_test.py \
+    --output $1
